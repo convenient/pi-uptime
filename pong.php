8c1eb4867f0674ee36bbcaeefeb13eb1098c72d0
Luke Rodgers <lukerodgers90@gmail.com>
2021-02-05 15:53:33 +0000

    Update pong.php

--- pong.php
+++ pong.php
@@ -6,8 +6,10 @@ set_error_handler(function ($severity, $message, $file, $line) {
 $data = json_decode(file_get_contents(__DIR__ . '/data.json'), true);
 
 $lastPush = $data['timestamp'];
+echo "lastPush at $lastPush" . PHP_EOL;
 
 $sixHoursAgo = strtotime('-2 hours');
+echo "timeAgo at $sixHoursAgo" . PHP_EOL;
 if ($lastPush <= $sixHoursAgo) {
     echo "failure" . PHP_EOL;
     exit(1);
